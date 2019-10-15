d1004a5653d48e9fc5e517347568d560a1eb3c58
weicong <weicong@xindong.com>
2019-10-15 15:08:04 +0800

    更新go注释等

--- src/team/main/page13/main.go
+++ src/team/main/page13/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"math"
 	"fmt"
+	"math"
 )
 
 func main() {
-	// 个人理解为,hypot成为他的函数名.
+	// 个人理解为,hypot成为他的函数名. 其实，hypot是一个指针，指向堆里的函数体。
 	// 函数也是值。Function values
 	hypot := func(x, y float64) float64 {
 		return math.Sqrt(x*x + y*y)
--- src/team/main/page7/main2.go
+++ src/team/main/page7/main2.go
@@ -2,19 +2,23 @@ package main
 
 import "fmt"
 
+/**
+斐波那契数
+*/
 func fib() func() int {
 	a, b := 0, 1
-	c:=func() int {
+	c := func() int {
 		a, b = b, a+b
 		return a
 	}
 	return c
 }
 
+// 函数内部函数。每次初始化，将会初始化外部的函数内变量值
 func main() {
 	f := fib()
-	fmt.Println(f(), f(), f(), f(), f(),f())
-	d:=fib()()
-	fmt.Println(d,d,d,d,d,d)
-	fmt.Println(fib()(),fib()(),fib()(),fib()(),fib()())
+	fmt.Println(f(), f(), f(), f(), f(), f())
+	d := fib()()
+	fmt.Println(d, d, d, d, d, d)
+	fmt.Println(fib()(), fib()(), fib()(), fib()(), fib()())
 }
