22a0f09c5a585dee88fcf04c5db90e458f12c3b3
weicong <weicong@xindong.com>
2019-10-15 15:46:12 +0800

    2222

--- src/team/main/page14/main.go
+++ src/team/main/page14/main.go
@@ -45,6 +45,7 @@ func main() {
 	// 没有条件的 switch 同 `switch true` 一样。
 	// 这一构造使得可以用更清晰的形式来编写长的 if-then-else 链。
 	switch {
+
 	case t.Hour() < 12:
 		fmt.Println("Good morning!")
 	case t.Hour() < 17:
