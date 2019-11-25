32b4484121413d17abd764ad74ea6433e0f7d4af
Wh1t3zZ <13821701853@163.com>
2019-11-25 10:48:21 +0800

    更新 rabbitmq调研
    Signed-off-by: Wh1t3zZ <13821701853@163.com>

--- src/team2/main/main.go
+++ src/team2/main/main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
-	//"team2/io/file"
-	//"team2/defer"
-	"team2/io/file"
+	"github.com/zxbit2011/gifCaptcha"
+	"image/color"
+	"image/gif"
+	"net/http"
 )
+
+var cap1 = gifCaptcha.New()
+
 func main() {
-	//=================file================//
-	file.File()
-	//=================defer===============//
-	//_defer.Defer()
+	//设置颜色
+	cap1.SetFrontColor(color.Black, color.RGBA{255, 0, 0, 255}, color.RGBA{0, 0, 255, 255}, color.RGBA{0, 153, 0, 255})
+	cap1.Create(4, gifCaptcha.NUM)
+	http.HandleFunc("/img", func(w http.ResponseWriter, r *http.Request) {
+		gifData, code := cap1.RangCaptcha()
+		println(code)
+		gif.EncodeAll(w, gifData)
+	})
+	http.ListenAndServe(":8080", nil)
 }
