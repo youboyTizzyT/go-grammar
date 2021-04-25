9fa3caaeaaa9754a63615f45e9f3651f895e4bf9
weicong <weicong@xindong.com>
2021-04-25 12:54:03 +0800

    更新 线程模型性能测试用例

--- problem/goRountine/goRountineModel_test.go
+++ problem/goRountine/goRountineModel_test.go
@@ -1,7 +1,6 @@
 package myTest
 
 import (
-	"fmt"
 	"sort"
 	"sync"
 	"testing"
@@ -10,59 +9,17 @@ import (
 
 var wg = sync.WaitGroup{}
 
-func TestThread1(t *testing.T) {
-	for i := 0; i < 1; i++ {
-		go forRun(int32(i))
-	}
-	time.Sleep(1 * time.Second)
-	wg.Wait()
-}
-func TestThread2(t *testing.T) {
-	for i := 0; i < 2; i++ {
-		go forRun(int32(i))
-	}
-	time.Sleep(1 * time.Second)
-	wg.Wait()
-}
-func TestThread3(t *testing.T) {
-	for i := 0; i < 3; i++ {
-		go forRun(int32(i))
-	}
-	time.Sleep(1 * time.Second)
-	wg.Wait()
-}
-func TestThread4(t *testing.T) {
-	for i := 0; i < 4; i++ {
-		go forRun(int32(i))
-	}
-	time.Sleep(1 * time.Second)
-	wg.Wait()
-}
-func TestThread8(t *testing.T) {
-	for i := 0; i < 8; i++ {
-		go forRun(int32(i))
-	}
-	time.Sleep(1 * time.Second)
-	wg.Wait()
-}
-func TestThread12(t *testing.T) {
-	for i := 0; i < 12; i++ {
-		go forRun(int32(i))
-	}
-	time.Sleep(1 * time.Second)
-	wg.Wait()
-}
-func TestThread16(t *testing.T) {
-	for i := 0; i < 16; i++ {
-		go forRun(int32(i))
-	}
-	time.Sleep(1 * time.Second)
-	wg.Wait()
-}
+func BenchmarkThread1(t *testing.B)  { forMain(t, 1) }
+func BenchmarkThread2(t *testing.B)  { forMain(t, 2) }
+func BenchmarkThread4(t *testing.B)  { forMain(t, 4) }
+func BenchmarkThread6(t *testing.B)  { forMain(t, 6) }
+func BenchmarkThread8(t *testing.B)  { forMain(t, 8) }
+func BenchmarkThread12(t *testing.B) { forMain(t, 12) }
+func BenchmarkThread16(t *testing.B) { forMain(t, 16) }
 
-func TestThread20(t *testing.T) {
-	for i := 0; i < 200; i++ {
-		go forRun(int32(i))
+func forMain(t *testing.B, int2 int) {
+	for i := 0; i < int2; i++ {
+		go forRun(int32(t.N))
 	}
 	time.Sleep(1 * time.Second)
 	wg.Wait()
@@ -75,30 +32,31 @@ func forRun(num int32) {
 	wg.Add(1)
 	defer wg.Done()
 	var echoFunc = make(times, 0)
-	start := time.Now()
-	for i := 0; i < 1000; i++ {
-		echoFunc = runFunc(echoFunc)
-		time.Sleep(1 * time.Millisecond)
+	//start := time.Now()
+	for i := 0; i < int(num); i++ {
+		runFunc(echoFunc)
+		//echoFunc = runFunc(echoFunc)
+		//time.Sleep(1 * time.Millisecond)
 	}
-	fmt.Println("总共耗时", num, time.Now().Sub(start))
+	//fmt.Println("总共耗时", num, time.Now().Sub(start))
 	aa := time.Duration(0)
 	for _, duration := range echoFunc {
 		aa += duration
 	}
 	sort.Sort(echoFunc)
-	fmt.Println("平均延迟", num, aa/time.Duration(len(echoFunc)))
-	fmt.Println("top 5 Max延迟", num, echoFunc[len(echoFunc)-5:])
-	fmt.Println("top 5 Min延迟", num, echoFunc[:5])
+	//fmt.Println("平均延迟", num, aa/time.Duration(len(echoFunc)))
+	//fmt.Println("top 5 Max延迟", num, echoFunc[len(echoFunc)-5:])
+	//fmt.Println("top 5 Min延迟", num, echoFunc[:5])
 }
 
 //========================================================
 /*
 执行方法
 */
-func runFunc(echoFunc []time.Duration) []time.Duration {
-	start := time.Now()
+func runFunc(echoFunc []time.Duration) {
+	//start := time.Now()
 	feibonaqie(31)
-	return append(echoFunc, time.Now().Sub(start))
+	//return
 }
 
 /*
