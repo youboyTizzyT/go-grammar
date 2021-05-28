bd409e5d971eaed38722fb58917798ea10246d54
Wh1t3zZ <13821701853@163.com>
2021-05-28 16:30:47 +0800

    上传go协程例子

--- problem/goRountine/test2/main.go
+++ problem/goRountine/test2/main.go
@@ -6,23 +6,23 @@ import (
 	"time"
 )
 
-/*
-#define _GNU_SOURCE
-#include <sched.h>
-*/
-import "C"
+///*
+//#define _GNU_SOURCE
+//#include <sched.h>
+//*/
+//import "C"
 
 func worker(id int) {
 	start := time.Now()
 	for i := 0; i < 1000; i++ {
-		fmt.Printf("worker: %d, CPU: %d\n", id, C.sched_getcpu())
+		//fmt.Printf("worker: %d, CPU: %d\n", id, C.sched_getcpu())
 		//feibonaqie(3)
 		//feibonaqie(20)
 		//feibonaqie(31)
 		//feibonaqie(32)
 		feibonaqie(33)
 	}
-	fmt.Printf("worker: %d, costTime: %d\n", id, time.Now().Sub(start))
+	fmt.Printf("worker: %d, costTime: %v\n", id, time.Now().Sub(start))
 }
 
 func feibonaqie(int2 int) int {
@@ -39,8 +39,9 @@ func feibonaqie(int2 int) int {
 }
 
 func main() {
-	for i := 0; i < runtime.NumCPU(); i++ {
-		go worker(i)
+	runtime.GOMAXPROCS(1)
+	for i := 0; i < 2; i++ {
+		go worker(1)
 	}
-	time.Sleep(2 * time.Second)
+	time.Sleep(20 * time.Second)
 }
